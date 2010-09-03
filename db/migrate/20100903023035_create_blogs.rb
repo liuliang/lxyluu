c1dc15eff5812c829b3f6925335d238dc73d7f56
liang.liu <liu_liang_@126.com>
2010-09-03 10:46:51 +0800

    fck

--- db/migrate/20100903023035_create_blogs.rb
+++ db/migrate/20100903023035_create_blogs.rb
@@ -1,7 +1,12 @@
 class CreateBlogs < ActiveRecord::Migration
   def self.up
     create_table :blogs do |t|
-
+      t.string :title, :default => nil
+      t.text :content, :default => nil
+      t.integer :flag, :default => 0
+      t.integer :blog_category_id
+      t.string :tags, :default=>nil
+      t.boolean :is_private, :default=>false
       t.timestamps
     end
   end
